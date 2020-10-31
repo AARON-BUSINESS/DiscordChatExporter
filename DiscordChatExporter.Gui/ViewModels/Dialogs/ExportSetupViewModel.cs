b40cd39d638934b609d921ade68404be17ee4535
Oscar <48969906+OscarRed@users.noreply.github.com>
2020-10-31 23:10:17 +1300

    Update ExportSetupViewModel.cs

--- DiscordChatExporter.Gui/ViewModels/Dialogs/ExportSetupViewModel.cs
+++ DiscordChatExporter.Gui/ViewModels/Dialogs/ExportSetupViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 using DiscordChatExporter.Domain.Discord.Models;
 using DiscordChatExporter.Domain.Exporting;
 using DiscordChatExporter.Gui.Services;
